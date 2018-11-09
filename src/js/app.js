2a6c8e1308ab6ad190d4e1563cbe64b40062a6a3
achiadg <achiadg@gmail.com>
2018-11-09 19:08:56 +0200

    test

--- src/js/app.js
+++ src/js/app.js
@@ -5,6 +5,33 @@ $(document).ready(function () {
     $('#codeSubmissionButton').click(() => {
         let codeToParse = $('#codePlaceholder').val();
         let parsedCode = parseCode(codeToParse);
-        $('#parsedCode').val(JSON.stringify(parsedCode, null, 2));
+        let codeAfterParse = restrictElements(parsedCode);
+        console.log(codeAfterParse);
+        //$('#parsedCode').val(JSON.stringify(parsedCode, null, 2));
+        var table = document.getElementById('main-table');
+        addLine(table, 'var1', 'var2', 'var3', 'var4');
+
     });
 });
+
+function restrictElements(parsedCode) {
+    if(parsedCode){
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+function addLine(table, v1, v2, v3, v4) {
+    var count = $('#main-table').length;
+    var row = table.insertRow(count);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+    cell1.innerHTML = v1;
+    cell2.innerHTML = v2;
+    cell3.innerHTML = v3;
+    cell4.innerHTML = v4;
+}
